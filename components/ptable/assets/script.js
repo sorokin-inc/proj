47c30bfdd19b9e14727bc32cb967c03f3f92adb1
Nick S <mycytasorokin@gmail.com>
2020-01-20 19:22:58 +0200

    extention

--- components/ptable/assets/script.js
+++ components/ptable/assets/script.js
@@ -1,8 +1,32 @@
-if (document.documentElement.clientHeight > document.documentElement.clientWidth) {
-    var ask = confirm('Flip your devise or unfold your browser window to fullscreen');
-    screen.orientation === "landscape-primary";
+function fold() {
+    breaker = document.getElementById('breaker');
+    contentBx = document.getElementById('contentBx');
+    folder = document.getElementById('folder');
+    isChecked = (folder.checked) ? true : false;
+    if (folder.checked) {
+        breaker.className = "actBr";
+        contentBx.className = "actContentBx";
+    } else {
+        breaker = document.getElementById('breaker');
+        contentBx = document.getElementById('contentBx');
+        folder = document.getElementById('folder').value;
+        breaker.className = "breaker";
+        contentBx.className = "contentBx";
+    }
 }
-
-window.addEventListener("orientationchange", function() {
-	alert(screen.orientation);
-}, false);
+function minimize() {
+    breaker = document.getElementById('breaker');
+    contentBx = document.getElementById('contentBx');
+    minimizer = document.getElementById('minimizer');
+    isChecked = (minimizer.checked) ? true : false;
+    if (minimizer.checked) {
+        breaker.className = "minBr";
+        contentBx.className = "minContentBx";
+    } else {
+        breaker = document.getElementById('breaker');
+        contentBx = document.getElementById('contentBx');
+        folder = document.getElementById('folder').value;
+        breaker.className = "breaker";
+        contentBx.className = "contentBx";
+    }
+}
