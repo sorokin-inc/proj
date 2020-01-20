6739061ac39f99199690f32695d424676525c7d3
Nick S <mycytasorokin@gmail.com>
2020-01-20 12:03:38 +0200

    script

--- components/ptable/assets/script.js
+++ components/ptable/assets/script.js
@@ -1,5 +1,6 @@
 if (document.documentElement.clientHeight > document.documentElement.clientWidth) {
     var ask = confirm('Flip your devise or unfold your browser window to fullscreen');
+    screen.orientation === "landscape-primary";
 }
 
 window.addEventListener("orientationchange", function() {
