6d7fa69f056bd01fe95835a73f5bdea82657b766
Nick S <mycytasorokin@gmail.com>
2020-01-21 20:15:21 +0200

    php dev

--- components/php/index.php
+++ components/php/index.php
@@ -1,12 +1,11 @@
 <?php
 require('elements/elem_db.php');
+require('elements/table.php');
 
 $id = $_GET['elem_id'];
 $title = get_elem_title($id);
 $content = get_elem_content($id);
 
-require('elements/table.php');
-require('elements/cover.php');
 require('elements/breaker.php');
 
-index.php?>
+?>
